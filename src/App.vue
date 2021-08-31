3320597538c95803a638ebee84c4691ee4a218cb
niuniu315 <454378439@qq.com>
2021-08-31 23:50:19 +0800

    money布局，引入全局样式和变量

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,21 @@
 <template>
-  <div>
+  <div id="app">
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
-  * {
-    margin: 0; padding: 0;
-    box-sizing: border-box;
-  }
+  @import "assets/style/helper.scss";
+  @import "assets/style/reset.scss";
+
   body{
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #333;
+    font-family: $font-hei;
     line-height: 1.5;
   }
-  a{
-    text-decoration: none;
-    color: inherit;
-  }
+
   #app {
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -22,11 +22,14 @@
 </script>
 
 <style lang="scss" scoped>
+  @import "~@/assets/style/helper.scss";
+
   nav {
     display: flex;
     box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
     flex-direction: row;
     font-size: 12px;
+
     > .item {
       padding: 2px 0;
       width: 33.33333%;
@@ -34,13 +37,15 @@
       justify-content: center;
       align-items: center;
       flex-direction: column;
+
       .icon {
         width: 32px;
         height: 32px;
       }
     }
-    > .item.selected{
-      color: red;
+
+    > .item.selected {
+      color: $color-highlight;
     }
   }
 </style>
