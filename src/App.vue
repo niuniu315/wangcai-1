687116ea29d01dbf02d0d840c54ad82320e9bb6b
niuniu315 <454378439@qq.com>
2021-08-31 17:14:23 +0800

    更新icon

--- src/App.vue
+++ src/App.vue
@@ -5,28 +5,31 @@
 </template>
 
 <style lang="scss">
-  *{
-    margin: 0;
-    padding: 0;
+  * {
+    margin: 0; padding: 0;
     box-sizing: border-box;
   }
+  body{
+    line-height: 1.5;
+  }
+  a{
+    text-decoration: none;
+    color: inherit;
+  }
   #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
-    color: #2c3e50;
+    color: #2C3E50;
   }
-
   #nav {
     padding: 30px;
-
     a {
       font-weight: bold;
-      color: #2c3e50;
-
+      color: #2C3E50;
       &.router-link-exact-active {
-        color: #42b983;
+        color: #42B983;
       }
     }
   }
