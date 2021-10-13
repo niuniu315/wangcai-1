a2eb73e10cfcaa8eb4b447291475120ae6fc0f3b
niuniu315 <454378439@qq.com>
2021-10-13 23:41:36 +0800

    修复money 里 新建标签功能

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -7,7 +7,7 @@
                 placeholder="在这里输入备注"
                 @update:value="onUpdateNotes"/>
     </div>
-    <Tags :data-source.sync="tags" @update:value="onUpdateTags"/>
+    <Tags  />
   </Layout>
 </template>
 
@@ -25,15 +25,11 @@
   })
 
   export default class Money extends Vue {
-    tags = store.tagList;
     recordList = store.recordList;
     record: RecordItem = {
       tags: [], notes: '', type: '-', amount: 0
     };
 
-    onUpdateTags(value: string[]) {
-      this.record.tags = value;
-    }
 
     onUpdateNotes(value: string) {
       this.record.notes = value;
